737248c252d8e5c06b6a70c65868c843c323a588
Kaveri Priya Putti <kaveripriyap2312@gmail.com>
2022-05-08 08:16:30 +0530

    Update IPFS url +json file

--- main.py
+++ main.py
@@ -13,6 +13,8 @@ from pydantic import BaseModel
 from utils import *
 import json
 
+import ipfsApi
+
 tokenizer = AutoTokenizer.from_pretrained("distilgpt2")
 
 MODEL = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
@@ -33,21 +35,59 @@ async def root():
 async def generateImage(ModelInput: ModelInput):
     input = " ".join(ModelInput.__dict__)
 
-    input_tensor = tf.constant(tokenizer(input)['input_ids'])
-    input_tensor = tf.reshape(input_tensor, (input_tensor.shape[0], 1, 1))
-    tiled_tensor = tf.tile(input_tensor, tf.constant([430 // input_tensor.shape[0], 522, 3]))
+    tiled_tensor, img = tokenise(input)
+    tensor_to_image(tiled_tensor)
 
-    content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
-    style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
-
+    questionable_style_image = tf.cast(tf.reshape(tiled_tensor, [1] + tiled_tensor.shape), tf.float32)
+    content_path = "/content/raw-img"
+    style_path = "/content/musemart/dataset_updated/training_set"
     content_image = load_img(content_path)
     style_image = load_img(style_path)
 
-    mask_image = tf.cast(tf.reshape(tiled_tensor, [1] + tiled_tensor.shape), tf.float32)
+    plt.subplot(1, 2, 1)
+    imshow(content_image, 'Content Image')
 
-    prediction = MODEL(mask_image, style_image)[0]
-    prediction = MODEL(content_image, prediction)[0]
+    plt.subplot(1, 2, 2)
+    imshow(style_image, 'Style Image')
+
+    image = generate_image(questionable_style_image, content_image, style_image)
+
+    # connect to a public IPFS gateway
+    api = ipfsApi.Client(host='https://ipfs.infura.io', port=5001)
+
+    result = api.add(image)
+    display(result)
+    # get the hash of the image from the result variable obtained earlier
+    CID = result['Hash']
+    # get the image from ipfs
+    api.get(CID)
+
+    # load the image locally
+    img_PIL = PIL.Image.open(CID)
+    # display the image inline
+    display(img_PIL)
+
+    ipfsGateway = f"https://ipfs.io/ipfs/{CID}"
 
     return {
-        "prediction": json.dumps(prediction.numpy().tolist()), 
+            'name': ModelInput.name,
+            'school': ModelInput.school,
+            'linkedIn': ModelInput.linkedIn,
+            'github': ModelInput.github,
+            'image': ipfsGateway
     }
+
+    # input_tensor = tf.constant(tokenizer(input)['input_ids'])
+    # input_tensor = tf.reshape(input_tensor, (input_tensor.shape[0], 1, 1))
+    # tiled_tensor = tf.tile(input_tensor, tf.constant([430 // input_tensor.shape[0], 522, 3]))
+
+    # content_path = tf.keras.utils.get_file('YellowLabradorLooking_new.jpg', 'https://storage.googleapis.com/download.tensorflow.org/example_images/YellowLabradorLooking_new.jpg')
+    # style_path = tf.keras.utils.get_file('kandinsky5.jpg','https://storage.googleapis.com/download.tensorflow.org/example_images/Vassily_Kandinsky%2C_1913_-_Composition_7.jpg')
+
+    # content_image = load_img(content_path)
+    # style_image = load_img(style_path)
+
+    # mask_image = tf.cast(tf.reshape(tiled_tensor, [1] + tiled_tensor.shape), tf.float32)
+
+    # prediction = MODEL(mask_image, style_image)[0]
+    # prediction = MODEL(content_image, prediction)[0]
--- utils.py
+++ utils.py
@@ -1,5 +1,28 @@
 import tensorflow as tf 
 
+import io
+import os
+import re
+import shutil
+import string
+import tensorflow as tf
+import random
+
+import IPython.display as display
+
+import matplotlib.pyplot as plt
+import matplotlib as mpl
+mpl.rcParams['figure.figsize'] = (12, 12)
+mpl.rcParams['axes.grid'] = False
+
+import numpy as np
+import PIL.Image
+import time
+import functools
+from transformers import AutoTokenizer, AutoModelForCausalLM
+import tensorflow_hub as hub
+hub_model = hub.load('https://tfhub.dev/google/magenta/arbitrary-image-stylization-v1-256/2')
+
 def tensor_to_image(tensor):
   tensor = tensor*255
   tensor = np.array(tensor, dtype=np.uint8)
@@ -40,3 +63,67 @@ def load_img(path_to_img):
   img = img[tf.newaxis, :]
   return img
 
+def tensor_to_image(tensor):
+  tensor = tensor*255
+  tensor = np.array(tensor, dtype=np.uint8)
+  if np.ndim(tensor)>3:
+    assert tensor.shape[0] == 1
+    tensor = tensor[0]
+  return PIL.Image.fromarray(tensor)
+
+def tokenise(sentence):
+  tensor = tf.constant(tokenizer(sentence)['input_ids'])
+  arr = tensor.numpy()
+  dim = 500 * 522 * 3
+  mul = (dim // arr.shape[0]) + 1
+  arr = np.repeat(arr, mul)
+  arr = arr[:dim]
+  arr = np.random.permutation(arr)
+  tensor = tf.constant(arr)
+  tensor = tf.reshape(tensor, ([500,522,3])) 
+  tiled_tensor = tensor % 255
+
+  img = tensor_to_image(tiled_tensor)
+  return tiled_tensor, img
+
+def load_img(path_to_img):
+  parent_folder = os.listdir(path_to_img)
+  folder = random.choice(parent_folder)
+  path = path_to_img + "/" + folder
+  images = os.listdir(path)
+  img = random.choice(images)
+  path_to_img = path + "/" + img
+
+  max_dim = 512
+  img = tf.io.read_file(path_to_img)
+  img = tf.image.decode_image(img, channels=3)
+  img = tf.image.convert_image_dtype(img, tf.float32)
+
+  shape = tf.cast(tf.shape(img)[:-1], tf.float32)
+  long_dim = max(shape)
+  scale = max_dim / long_dim
+
+  new_shape = tf.cast(shape * scale, tf.int32)
+
+  img = tf.image.resize(img, new_shape)
+  img = img[tf.newaxis, :]
+  return img
+
+def imshow(image, title=None):
+  if len(image.shape) > 3:
+    image = tf.squeeze(image, axis=0)
+
+  plt.imshow(image)
+  if title:
+    plt.title(title)
+
+def generate_art(questionable_style_image, content_image, style_image):
+  stylized_image = hub_model(questionable_style_image, tf.constant(style_image))[0]
+  alien_splatter = hub_model(tf.constant(content_image), stylized_image)[0]
+  return alien_splatter
+
+def generate_image(questionable_style_image, content_image, style_image):
+  art = generate_art(questionable_style_image, content_image, style_image)
+  art = tensor_to_image(art)
+  im1 = art.save("art.png", "PNG")
+  return im1
